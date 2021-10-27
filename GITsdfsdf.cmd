3c33a6018ae6ffb7883cad050b52673040cba39f
Cristian Mota <scriptadore@gmail.com>
2021-10-27 06:07:30 -0300

    first commit

---
+++ GITsdfsdf.cmd
@@ -0,0 +1,32 @@
+
+git init
+
+ping 0 -n 4 >nul
+
+cd C:\Users\HUNTER\Desktop\teste\Rep
+
+pause
+
+git add .
+
+pause
+
+git commit -m "first commit"
+
+pause
+
+rem git branch -M main
+
+pause
+
+git remote add origin https://github.com/cristian-mota/Rep.git
+
+rem git clone https://github.com/cristian-mota/Rep.git
+
+
+
+pause
+
+git push --set-upstream origin master
+
+pause
